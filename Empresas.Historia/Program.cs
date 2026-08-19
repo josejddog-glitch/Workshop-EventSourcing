da50cd7fa4479c15db2e560e491e685d0092a5a8
Jddog123 <daniel.uruena@sinco.com.co>
2026-08-19 13:03:06 -0500

    No se sabria que handelr o comando jejecutar si noe sta rtipado con record

--- Empresas.Historia/Program.cs
+++ Empresas.Historia/Program.cs
@@ -1,15 +1,33 @@
 ﻿var stream = new EventStream<Empresa>();
+
+//Handler
 var registrarEmpresaHandler = new RegistrarEmpresaHandler(stream);
 var cambiarPlanHandler = new CambiarPlanHandler(stream);
 var suspenderHandler = new SuspenderHandler(stream);
 var reactivarHandler = new ReactivarHandler(stream);
 
-registrarEmpresaHandler.Handle("Constructora Andes", "Basico");
-suspenderHandler.Handle("Falta de pago");
-reactivarHandler.Handle();
-cambiarPlanHandler.Handle("Premium");
-suspenderHandler.Handle("Incumplimiento de contrato");
-reactivarHandler.Handle();
+//Commands
+var commandoRegistrarEmpresa = new RegistrarEmpresa("Constructora Andes", "Basico");
+var commandoSuspenderNoPago = new Suspender("Falta de pago");
+var commandoReactivarUno= new Reactivar();
+var commandoCambiarPlan= new CambiarPlan("Premium");
+var commandoSuspenderIncumplimiento = new Suspender("Falta de pago");
+var commandoReactivarDos= new Reactivar();
+
+//Register command handler
+var desparchador = new Despachador();
+desparchador.Registrar<RegistrarEmpresa>(registrarEmpresaHandler);
+desparchador.Registrar(suspenderHandler);
+desparchador.Registrar(reactivarHandler);
+desparchador.Registrar(cambiarPlanHandler);
+
+//Enviar command
+desparchador.Enviar(commandoRegistrarEmpresa);
+desparchador.Enviar(commandoSuspenderNoPago);
+desparchador.Enviar(commandoReactivarUno);
+desparchador.Enviar(commandoCambiarPlan);
+desparchador.Enviar(commandoSuspenderIncumplimiento);
+desparchador.Enviar(commandoReactivarDos);
 
 var andes2Version = stream.Get();
 
@@ -93,30 +111,35 @@ public class EventStream<T> where T : AgregateRoot, new()
     }
 }
 
-public class RegistrarEmpresaHandler(EventStream<Empresa> stream)
+public interface ICommandHandler<TCommand>
+{ 
+    public void Handle(TCommand command);
+}
+
+public class RegistrarEmpresaHandler(EventStream<Empresa> stream) : ICommandHandler<RegistrarEmpresa>
 {
-    public void Handle(string nombre, string plan)
+    public void Handle(RegistrarEmpresa registrarEmpresa)
     {
         var andes = stream.Get();
-        stream.Append(andes.Registrar(nombre, plan));
+        stream.Append(andes.Registrar(registrarEmpresa.Nombre, registrarEmpresa.Plan));
     }
 }
 
-public class CambiarPlanHandler(EventStream<Empresa> stream)
+public class CambiarPlanHandler(EventStream<Empresa> stream) : ICommandHandler<CambiarPlan>
 {
-    public void Handle(string plan)
+    public void Handle(CambiarPlan cambiarPlan)
     {
         var andes = stream.Get();
-        stream.Append(andes.CambiarPlan(plan));
+        stream.Append(andes.CambiarPlan(cambiarPlan.plan));
     }
 }
 
-public class SuspenderHandler(EventStream<Empresa> stream)
+public class SuspenderHandler(EventStream<Empresa> stream) : ICommandHandler<Suspender>
 {
-    public void Handle(string motivo)
+    public void Handle(Suspender suspender)
     {
         var andes = stream.Get();
-        var empresaSuspendida2 = andes.Suspender(motivo);
+        var empresaSuspendida2 = andes.Suspender(suspender.motivo);
         if(empresaSuspendida2 is not null)
         {
             stream.Append(empresaSuspendida2); 
@@ -124,13 +147,21 @@ public class SuspenderHandler(EventStream<Empresa> stream)
     }
 }
 
-public class ReactivarHandler(EventStream<Empresa> stream)
+public class ReactivarHandler(EventStream<Empresa> stream) : ICommandHandler<Reactivar>
 {
-    public void Handle()
+    public void Handle(Reactivar reactivar)
     {
         var andes = stream.Get();
         stream.Append(andes.Reactivar());
     }
+}
+
+public class Despachador
+{
+    private Dictionary<Type , Action<object>> handlers = new();
+    public void Registrar<T>(ICommandHandler<T> handler) => handlers.Add(typeof(T), (object command) => handler.Handle((T)command));
+
+    public void Enviar(object command) => handlers[command.GetType()](command);
 }
 
 //eventos
@@ -138,3 +169,9 @@ public record EmpresaRegistrada(string Nombre, string Plan);
 public record PlanCambiado(string PlanNuevo);
 public record EmpresaSuspendida(string Motivo);
 public record EmpresaReactivada();
+
+//commands
+public record RegistrarEmpresa(string Nombre, string Plan);
+public record CambiarPlan(string plan);
+public record Suspender(string motivo);
+public record Reactivar();
